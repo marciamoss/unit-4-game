452da704a37e2a53c4cb9ecf8de359f04f5b5638
Autumn4eva <45056799+marciamoss@users.noreply.github.com>
2018-12-29 11:06:28 -0500

    Crystals Collector functional game

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # unit-4-game
+Crystals Collector Game:
+
+ Here's how the app works:
+
+   * There will be four crystals displayed as buttons on the page.
+
+   * The player will be shown a random number at the start of the game.
+
+   * When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
+
+     * The game will hide this amount until the player clicks a crystal.
+     * When they do click one, the player's score counter is updated.
+
+   * The player wins if their total score matches the random number from the beginning of the game.
+
+   * The player loses if their score goes above the random number.
+
+   * The game restarts whenever the player wins or loses.
+
+     * When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. The user's score (and score counter) will reset to zero.
+
+   * The app shows the number of games the player wins and loses. 
+   * If the page is refreshed the game is restarted from the begining.
---
+++ assets/javascript/game.js
@@ -0,0 +1,105 @@
+// JavaScript function that wraps everything
+$(document).ready(function() {
+    //global vars
+    var crysNum=[];
+    var random;
+    var collectorValue;
+    var finalSum;
+    var win;
+    var los;
+    win=0;
+    los=0;
+
+    function reset(){
+        finalSum=0;
+        crysNum=[];
+        var final="Your Total Score is: "+finalSum;
+        $("#total").text(final);
+        begin();
+    }
+    
+    function clear(){
+        $("#randomNumber").empty();
+        $("#imgnum1").empty();
+        $("#imgnum2").empty();
+        $("#imgnum3").empty();
+        $("#imgnum4").empty();
+        $("#total").empty();
+        $("img").off("click");
+    }
+           
+    //function for random number generator 
+    function numGenerator(min,max){
+        random=Math.floor(Math.random() * (max+1-min)) + min;   
+    }
+
+    //function for generating random numbers between 1-12 for crystals
+    function poparray(){
+        numGenerator(1,12);
+        crysNum.push(random);
+    };
+
+    reset();
+    var wstats="Wins: "+win+"<br>"+"Losses: "+los;
+    $("#wGameStats").html(wstats);
+    var lstats="Losses: "+los;
+    $("#lGameStats").text(lstats);
+
+    function begin(){
+        //Generate random number 19-120
+        numGenerator(19,120);
+        //assign the value to the html page
+        collectorValue=random;
+        $("#randomNumber").text(collectorValue);
+     
+        //create crystal array without duplicate random numbers
+        while(crysNum.length<4){
+            poparray(); 
+            if (crysNum.length>1){
+                for( var z = 0; z < (crysNum.length-1); z++){
+                    lth=crysNum.length-1;
+                    if (crysNum[z]===crysNum[lth]){
+                        crysNum.pop();
+                    }    
+                }
+            }
+        }
+    
+        //assign the numbers to crystals
+        $("#imgnum1").attr("data-crystalvalue",crysNum[0]);
+        $("#imgnum2").attr("data-crystalvalue",crysNum[1]);
+        $("#imgnum3").attr("data-crystalvalue",crysNum[2]);
+        $("#imgnum4").attr("data-crystalvalue",crysNum[3]);
+
+        // var imageCrystal = $("<img>");
+        //imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+        $("img").on("click", function() {
+            var crystalValue = ($(this).attr("data-crystalvalue"));
+            crystalValue = parseInt(crystalValue);
+            finalSum=finalSum+crystalValue;
+            var final="Your Total Score is: "+finalSum;
+            $("#total").text(final);
+            if (finalSum===collectorValue){
+                win=win+1;
+                var wstats="You Win!!!"+"<br>"+"Wins: "+win+"<br>"+"Losses: "+los;
+                $("#wGameStats").html(wstats);
+                // var lstats="Losses: "+los;
+                // $("#lGameStats").text(lstats);
+                clear();
+                reset();
+                
+            }
+            if(finalSum>collectorValue){
+                los=los+1;
+                var wstats="You Lose!!!"+"<br>"+"Wins: "+win+"<br>"+"Losses: "+los;
+                $("#wGameStats").html(wstats);
+                // var lstats="Losses: "+los;
+                // $("#lGameStats").text(lstats);
+                clear();    
+                reset();
+            }
+        });
+
+};
+
+});
